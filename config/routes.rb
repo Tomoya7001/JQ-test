3d9ab57a6a7536375c851fa74e0ab78f3ae7e4e5
Tomoya7001 <spmiwdar7001@gmail.com>
2022-07-18 22:33:05 +0000

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  get 'lists/new'
+  post 'lists' => 'lists#create'
+  get 'lists' => 'lists#index'
+  get 'lists/:id' => "lists#show"
+  get 'lists/edit'
+  get 'top' => 'homes#top'
+end
