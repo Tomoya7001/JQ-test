c8f4c074ab2b70569e0918a65ff3ca322005c442
Tomoya7001 <spmiwdar7001@gmail.com>
2022-07-22 11:20:10 +0000

    投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  post 'lists' => 'lists#create'
-  get 'lists' => 'lists#index'
-  get 'lists/:id' => "lists#show"
-  get 'lists/edit'
+
   get 'top' => 'homes#top'
+  resources :lists
+
+
 end
