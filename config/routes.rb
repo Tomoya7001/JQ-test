b52705b3d388f8602292a73e3c3f424f6154b848
Tomoya7001 <spmiwdar7001@gmail.com>
2022-07-29 11:20:09 +0000

    devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   get 'top' => 'homes#top'
+  get "about" => "homes#about"
+  resources :conversations, only: [:index]
+
   resources :lists
 
 resources :conversations do
